db362cfe15f1c7e401df5aeb46c5a7fe969f4bc1
Dzikri Muhammad <125238574+Dzikri07@users.noreply.github.com>
2026-03-25 15:17:15 +0700

    pertemuan 2
    dzikri

---
+++ menuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class menuManager : MonoBehaviour
+{
+    public TMP_Text NamaText;
+
+    public void LoadScene(string NamaScene)
+    {
+        SceneManager.LoadScene(NamaScene);
+    }
+
+    public void SetNamaPanel(string NamaPanel)
+    {
+        NamaText.SetText(NamaPanel);
+    }
+
+    public void exit(){
+        Application.Quit();
+    }
+}
